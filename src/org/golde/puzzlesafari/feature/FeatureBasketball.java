4d5129adb5d61e83ff642ebfdc07b238272fb0d7
Eric Golde <eric@golde.org>
2020-05-29 23:42:52 -0700

    Somewhat of a wacky idea

--- src/org/golde/puzzlesafari/feature/FeatureBasketball.java
+++ src/org/golde/puzzlesafari/feature/FeatureBasketball.java
@@ -1,17 +1,45 @@
 package org.golde.puzzlesafari.feature;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.UUID;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Slime;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.material.MaterialData;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 public class FeatureBasketball extends FeatureBase {
 
+	@Override
+	public void onEnable() {
+		new BukkitRunnable() {
+			
+			@Override
+			public void run() {
+				doBallPhysics();
+			}
+		}.runTaskTimer(getPlugin(), 1, 1);
+	}
+	
 	@EventHandler
 	public void ballFiring(PlayerInteractEvent e) {
 		Player p = e.getPlayer();
@@ -20,23 +48,123 @@ public class FeatureBasketball extends FeatureBase {
 			return;
 		}
 
-		if(e.getItem().getType() == Material.CONCRETE_POWDER) {
-			fireSand(e.getPlayer());
+		if(e.getItem().getType() == Material.END_BRICKS) {
+			//fireSand(e.getPlayer());
+			fireSlime(e.getPlayer());
 		}
 
 
 	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+    public void eventRightClick(final PlayerInteractEntityEvent e) {
+        final Entity entity = e.getRightClicked();
+        if (entity instanceof Slime && this.balls.contains(entity)) {
+            final Slime slime = (Slime)entity;
+            slime.setVelocity(slime.getVelocity().add(new Vector(0, 1, 0).add(e.getPlayer().getLocation().getDirection().normalize().multiply(0.5).setY(0))));
+            slime.getWorld().playSound(slime.getLocation(), Sound.BLOCK_SLIME_HIT, 1.0f, 1.0f);
+        }
+    }
 
+	private HashSet<Slime> balls = new HashSet<Slime>();
+    private HashSet<UUID> ballIds = new HashSet<UUID>();
+    private HashMap<UUID, Vector> velocities  = new HashMap<UUID, Vector>();
 	
 	private void fireSand(Player player) {
 		World world = player.getWorld();
 		Location loc = player.getLocation().add(0, 2, 0);
-		FallingBlock falling = world.spawnFallingBlock(loc, new MaterialData(Material.CONCRETE, (byte)5));
+		FallingBlock falling = world.spawnFallingBlock(loc, new MaterialData(Material.END_BRICKS));
 		falling.setFallDistance(1);
-		
 		//Thanks crackle <3
 		falling.setVelocity(player.getLocation().getDirection().normalize().multiply(1.35));
 
 	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+    public void eventLoadChunk(final ChunkLoadEvent e) {
+        Entity[] entities;
+        for (int length = (entities = e.getChunk().getEntities()).length, i = 0; i < length; ++i) {
+            final Entity entity = entities[i];
+            final UUID id = entity.getUniqueId();
+            if (this.ballIds.contains(id) && entity instanceof Slime) {
+                this.ballIds.remove(id);
+                this.balls.add((Slime)entity);
+            }
+        }
+    }
+    
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void eventUnloadChunk(final ChunkUnloadEvent e) {
+        Entity[] entities;
+        for (int length = (entities = e.getChunk().getEntities()).length, i = 0; i < length; ++i) {
+            final Entity entity = entities[i];
+            if (entity instanceof Slime && this.balls.contains(entity)) {
+                this.ballIds.add(entity.getUniqueId());
+                this.balls.remove(entity);
+            }
+        }
+    }
+    
+    @EventHandler(priority = EventPriority.HIGH)
+    public void eventDamage(final EntityDamageEvent e) {
+        if (e.getEntity() instanceof Slime && this.balls.contains(e.getEntity()) && e.getCause() == EntityDamageEvent.DamageCause.FALL) {
+            e.setCancelled(true);
+        }
+    }
+	
+	private void fireSlime(Player p) {
+		final Location pos = p.getLocation();
+        final Slime ball = (Slime)p.getWorld().spawnEntity(pos.add(pos.getDirection().add(new Vector(0, 1, 0))), EntityType.SLIME);
+        ball.setRemoveWhenFarAway(false);
+        ball.setSize(1);
+        this.balls.add(ball);
+        ball.setVelocity(p.getLocation().getDirection().normalize().multiply(1.35));
+	}
+
+	
+	 public void doBallPhysics() {
+	        for (final Slime slime : this.balls) {
+	            final UUID id = slime.getUniqueId();
+	            Vector velocity = slime.getVelocity();
+	            if (this.velocities.containsKey(id)) {
+	                velocity = this.velocities.get(id);
+	            }
+	            if (slime.isDead()) {
+	                continue;
+	            }
+	            Boolean bounceSound = false;
+	            final Vector newv = slime.getVelocity();
+	            if (newv.getX() == 0.0) {
+	                newv.setX(-velocity.getX() * 0.8);
+	                if (Math.abs(velocity.getX()) > 0.3) {
+	                    bounceSound = true;
+	                }
+	            }
+	            else if (Math.abs(velocity.getX() - newv.getX()) < 0.1) {
+	                newv.setX(velocity.getX() * 0.98);
+	            }
+	            if (newv.getY() == -0.0784000015258789 && velocity.getY() < -0.1) {
+	                newv.setY(-velocity.getY() * 0.8);
+	                if (Math.abs(velocity.getY()) > 0.3) {
+	                    bounceSound = true;
+	                }
+	            }
+	            if (newv.getZ() == 0.0) {
+	                newv.setZ(-velocity.getZ() * 0.8);
+	                if (Math.abs(velocity.getZ()) > 0.3) {
+	                    bounceSound = true;
+	                }
+	            }
+	            else if (Math.abs(velocity.getZ() - newv.getZ()) < 0.1) {
+	                newv.setZ(velocity.getZ() * 0.98);
+	            }
+	            if (bounceSound) {
+	                slime.getWorld().playSound(slime.getLocation(), Sound.ENTITY_SLIME_SQUISH, 1.0f, 1.0f);
+	            }
+	            slime.setVelocity(newv);
+	            slime.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 1000000, -10, true), true);
+	            this.velocities.put(id, newv);
+	        }
+	 }
 
 }
--- src/org/golde/puzzlesafari/feature/FeatureParkour.java
+++ src/org/golde/puzzlesafari/feature/FeatureParkour.java
@@ -1,12 +1,16 @@
 package org.golde.puzzlesafari.feature;
 
+import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Guardian;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -19,38 +23,35 @@ import org.golde.puzzlesafari.utils.cuboid.MobCuboid;
 
 public class FeatureParkour extends FeatureBase {
 
-	private Cuboid health;
 	private Cuboid checkpoint;
 	private MobCuboid guardians;
-	
+
 	private static final int SPAWN_TICKS = 5;
 	private static final int AMOUNT_OF_GUARDIANS = 50;
 	private static final int CLEAR_TICKS = 20 * 60 * 15;
-	
+
 	@Override
 	public void onEnable() {
 		
-		
-		health = new Cuboid(new Location(getWorld(), 134, 10, 56), new Location(getWorld(), 134, 11, 62));
 		checkpoint = new Cuboid(new Location(getWorld(), 113, 4, 32), new Location(getWorld(), 118, 24, 86));
 		guardians = new MobCuboid(new Location(getWorld(), 64, 2, 32), new Location(getWorld(), 133, 2, 86));
-		
+
 		new EndCuboid(new Location(getWorld(), 135, 6, 56), new Location(getWorld(), 136, 6, 57), new EndCuboidCallback() {
-			
+
 			@Override
 			public void onEnter(Player p) {
 				sendFinishMessage(p, "Parkour", "the white wall");
 			}
 		});
-		
+
 		startTimers();
-		
-		
+
+
 	}
-	
+
 	private void startTimers() {
 		new BukkitRunnable() {
-			
+
 			@Override
 			public void run() {
 				if(guardians.getEntitiesInCuboid(Guardian.class).size() < AMOUNT_OF_GUARDIANS) {
@@ -61,7 +62,7 @@ public class FeatureParkour extends FeatureBase {
 				}
 			}
 		}.runTaskTimer(getPlugin(), 0, SPAWN_TICKS);
-		
+
 		new BukkitRunnable() {
 
 			@Override
@@ -73,14 +74,14 @@ public class FeatureParkour extends FeatureBase {
 
 			}
 		}.runTaskTimer(getPlugin(), 0, CLEAR_TICKS);
-		
+
 	}
 
 	@Override
 	public String getWarpTrigger() {
 		return "parkour";
 	}
-	
+
 	@Override
 	public void onEnter(Player p) {
 		sendEnterMessage(
@@ -91,17 +92,25 @@ public class FeatureParkour extends FeatureBase {
 				MOVEMENT_WASD
 				);
 	}
-	
+
 	@EventHandler
 	public void onMove(PlayerMoveEvent e) {
 		Player p = e.getPlayer();
-		if(health.inArea(p.getLocation())) {
-			p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 20, 10, true));
-		}
-		
 		if(checkpoint.inArea(p.getLocation())) {
 			WarpManager.setCheckpoint(p, "parkour2");
 		}
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onDoorInteract(PlayerInteractEvent e) {
+
+		if(e.getPlayer().getGameMode() != GameMode.CREATIVE) {
+			if (e.getClickedBlock().getType() == Material.BIRCH_DOOR) {
+				e.setCancelled(true);
+			}
+		}
+		
+
 	}
 
 }
